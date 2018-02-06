a7556822eee2559895637c5569a7e886964a6d47
Kent Brown <randall.k.brown.jr@gmail.com>
2018-02-06 11:45:13 -0500

    Created SmashballPane class. Created LoadingPane class for the LOADING state.

---
+++ src/com/smashball/java/main/view/pane/LoadingPane.java
@@ -0,0 +1,27 @@
+/* created by Kent Brown on 2/6/2018 */
+
+/* package */
+package com.smashball.java.main.view.pane;
+
+/* imports */
+
+import javafx.scene.image.Image;
+
+/* implementation */
+public class LoadingPane extends SmashballPane {
+
+    // STATE //
+
+    // CONSTRUCTOR //
+
+    // GETTERS & SETTERS //
+
+    // BEHAVIOUR //
+    @Override
+    void load() {
+        // Load the Background. //
+        Image img = new Image("someURL");
+
+    }
+
+}
---
+++ src/com/smashball/java/main/view/pane/SmashballPane.java
@@ -0,0 +1,24 @@
+/* created by Kent Brown on 2/6/2018 */
+
+/* package */
+package com.smashball.java.main.view.pane;
+
+/* imports */
+import javafx.scene.layout.Pane;
+
+/* implementation */
+public class SmashballPane extends Pane {
+
+    // STATE //
+
+    // CONSTRUCTOR //
+
+    // GETTERS & SETTERS //
+
+    // BEHAVIOUR //
+    void load() {
+        // Default load method, shared by all subclasses. //
+
+    }
+
+}
